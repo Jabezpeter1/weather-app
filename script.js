0041c1e2d497ffcf32caeb9563242c9b2278fbb9
Jabezpeter1 <59277708+Jabezpeter1@users.noreply.github.com>
2025-05-06 09:23:01 +0200

    Add apikey

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 async function getWeather() {
     const city = document.getElementById("cityInput").value;
-    const apiKey = "YOUR_API_KEY"; // Replace with your OpenWeatherMap API key
+    const apiKey = "8c489a48d22949fbe9ca7d8b7ee22543"; // Replace with your OpenWeatherMap API key
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   
     const response = await fetch(url);
